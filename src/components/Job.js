55c4c95837af91e75b677351d9f9dec9597225f0
Jaz Gobbo <jazzgobbo@gmail.com>
2022-04-02 15:39:11 -0500

    Styling job cards

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 
+
+const openLink = (url) => {
+    window.open(url)
+}
+
 const Job = ({ job }) => {
     return (
-        <div className='container-fluid'>
-            <div className="card" >
-                <div className='card-title'> {job.title} </div>
-                <div className='card-text'> {job.pay} </div>
-                <div> {job.term} </div>
-                <div> {job.location} </div>
+        <div className='container-fluid' onClick={() => openLink(job.link)}>
+            <div className="card">
+                <div className="card-title"> {job.title} </div>
+                <div className="card-body">
+                    <div className="row">
+                        <div className='card-text col-sm'> {job.pay} </div>
+                        <div className='card-text col-sm'> {job.term} </div>
+                        <div className='card-text col-sm'> {job.location} </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
