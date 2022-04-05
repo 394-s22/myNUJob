888d0e9017a311831a849fbcf22c793b6852fbfe
Brando Socarras <brandosocarras@gmail.com>
2022-04-04 21:02:16 -0500

    Init

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -17,26 +17,26 @@ const firebase = initializeApp(firebaseConfig);
 const database = getDatabase(firebase);
 
 export const useData = (path, transform) => {
-    const [data, setData] = useState();
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState();
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
 
-    useEffect(() => {
-      const dbRef = ref(database, path);
-      const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-      if (devMode) { console.log(`loading ${path}`); }
-      return onValue(dbRef, (snapshot) => {
-        const val = snapshot.val();
-        if (devMode) { console.log(val); }
-        setData(transform ? transform(val) : val);
-        setLoading(false);
-        setError(null);
-      }, (error) => {
-        setData(null);
-        setLoading(false);
-        setError(error);
-      });
-    }, [path, transform]);
+  useEffect(() => {
+    const dbRef = ref(database, path);
+    const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+    if (devMode) { console.log(`loading ${path}`); }
+    return onValue(dbRef, (snapshot) => {
+      const val = snapshot.val();
+      if (devMode) { console.log(val); }
+      setData(transform ? transform(val) : val);
+      setLoading(false);
+      setError(null);
+    }, (error) => {
+      setData(null);
+      setLoading(false);
+      setError(error);
+    });
+  }, [path, transform]);
 
-    return [data, loading, error];
-  };
+  return [data, loading, error];
+};
