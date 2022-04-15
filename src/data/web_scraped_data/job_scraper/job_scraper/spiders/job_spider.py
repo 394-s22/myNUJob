4d44513412b0ca4d769bcdb4b6112404f15b780c
gracewainaina <47871572+gracewainaina@users.noreply.github.com>
2022-04-14 21:27:31 -0500

    make dict of dict job title and description

--- src/data/web_scraped_data/job_scraper/job_scraper/spiders/job_spider.py
+++ src/data/web_scraped_data/job_scraper/job_scraper/spiders/job_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import json
 
 class JobSpiderSpider(scrapy.Spider):
     
@@ -6,15 +7,19 @@ class JobSpiderSpider(scrapy.Spider):
     allowed_domains = ['undergradaid.northwestern.edu']
     start_urls = ['https://undergradaid.northwestern.edu/work-study/jobs/on-campus-jobs/american-studies-newsletter-creation.html']
     
-    def __init__(self):
-        self.jobs = None
+    # def __init__(self):
+    #     self.jobs = None
 
     def parse(self, response):
         job_title = response.xpath('//div[@class="content"]//h1//text()').extract()[0]
         job_details = response.xpath('//div[@class="content"]//p//text()').extract()
 
+        job_dict = {job_title: {}}
+
         job_details = self.parse_helper(job_details)
-        return {job_title:job_details}
+        self.parse_job_json(job_details, job_dict[job_title])
+
+        return job_dict
     
     def parse_helper(self, job_details):
         job_details = [j for j in job_details if j != ': ' if j != ':' if j != '\xa0']
@@ -28,14 +33,24 @@ class JobSpiderSpider(scrapy.Spider):
                 job_details[idx] = new_job
         return job_details
 
-# {'American Studies: Newsletter Creation': ['TERM AVAILABLE', ' Spring 2022', 
-#                                         'NUMBER OF POSITIONS AVAILABLE', ' 1', 
-#                                         'LOCATION', 'Northwestern - Evanston Campus', 
-#                                         'WORK ARRANGEMENTS', ' Remote', 
-#                                         'DEPARTMENT', 'American Studies', 
-#                                         'JOB DESCRIPTION', 'To create a weekly newsletter for the students in the program to keep them informed about upcoming events and other things happening on campus that may be of interest.', 
-#                                         'QUALIFICATIONS', 'Must be a Northwestern undergraduate student with a Federal Work-Study allotment for the 2021-2022 academic year (CAESAR > Financial Aid > View My Financial Aid > [YEAR].)', 
-#                                         'PAY RATE', ' $18.00/hr', 
-#                                         'CONTACT NAME', 'Julie Lavin', 
-#                                         'CONTACT PHONE NUMBER', '(847) 467-2262', 
-#                                         'CONTACT EMAIL', 'julie.lavin@northwestern.edu']}
+    def parse_job_json(self, job_details, job_dict):
+        for idx, elem in enumerate(job_details):
+            # even indices are the keys, odd indices are the pairs to the job description
+            if (idx%2 == 0):
+                job_dict[elem] = job_details[idx+1]
+
+
+# output json object
+# {'American Studies: Newsletter Creation': {'TERM AVAILABLE': ' Spring 2022', 
+#                                         'NUMBER OF POSITIONS AVAILABLE': ' 1', 
+#                                         'LOCATION': 'Northwestern - Evanston Campus', 
+#                                         'WORK ARRANGEMENTS': ' Remote', 
+#                                         'DEPARTMENT': 'American Studies', 
+#                                         'JOB DESCRIPTION': 'To create a weekly newsletter for the students in the program to keep them informed about upcoming events and other things happening on campus that may be of interest.', 
+#                                         'QUALIFICATIONS': 'Must be a Northwestern undergraduate student with a Federal Work-Study allotment for the 2021-2022 academic year (CAESAR > Financial Aid > View My Financial Aid > [YEAR].)', 
+#                                         'PAY RATE': ' $18.00/hr', 
+#                                         'CONTACT NAME': 'Julie Lavin', 
+#                                         'CONTACT PHONE NUMBER': '(847) 467-2262', 
+#                                         'CONTACT EMAIL': 'julie.lavin@northwestern.edu'}}
+
+
