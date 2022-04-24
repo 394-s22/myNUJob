cc5582495c9714abbcbcde23c43d03a816cfe8da
Bobo Tam <bobo513786356@gmail.com>
2022-04-24 17:31:26 -0500

    graceLiu+bobo modal display information

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,19 +1,57 @@
-import React from 'react'
+
+import React, { useState } from 'react';
 import '../styles/Job.css'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 const openLink = (url) => {
     window.open(url)
 }
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 800,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
 const Job = ({ job }) => {
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <div className='container-fluid' >
             <div className="card">
                 <div className="card-title"> {job.TITLE}
+
                     <button className='apply-button'
-                        onClick={() => openLink(job.URL)}>
+                        onClick={handleOpen}>
                         <b>More Info</b>
                     </button>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                            {Object.entries(job).map(([key, value]) => {
+                                return (
+                                    <div>
+                                        {key}:{value}
+                                    </div>)})}
+                        </Box>
+                    </Modal>
+
                 </div>
                 <div className="card-category">
                     {job.DEPARTMENT}
@@ -21,7 +59,7 @@ const Job = ({ job }) => {
 
                 <div className="card-body">
                     <div className="row">
-                        <div className='card-text col-sm'> ${job["PAY RATE"]}/hr </div>
+                        <div className='card-text col-sm'> ${job["PAY RATE"][0]}/hr </div>
                         <div className='card-text col-sm'> {job["TERM AVAILABLE"]} </div>
                         <div className='card-text col-sm'> {job.LOCATION} </div>
                     </div>
