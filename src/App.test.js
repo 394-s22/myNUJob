ce65fd2def8fe70e085079e0bfe3be3cf55c3868
Brando Socarras <brandosocarras@gmail.com>
2022-05-25 10:10:58 -0500

    Added imports to app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 test('renders learn react link', () => {
