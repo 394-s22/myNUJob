4113482c6b91508cb29be2a28a62a354213fff45
Grace Liu <gxliu1@gmail.com>
2022-05-26 01:33:17 -0500

    fix jest unit test

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
-import './App.css';
-import React, { useState } from 'react';
-import JobList from './components/JobList.js';
-import FilterMenu from './components/FilterMenu.js';
-import NavBar from './components/NavBar.js';
-import SortBar from './components/SortBar';
+import "./App.css";
+import React, { useState } from "react";
+import JobList from "./components/JobList.js";
+import FilterMenu from "./components/FilterMenu.js";
+import NavBar from "./components/NavBar.js";
+import SortBar from "./components/SortBar";
 
-import { useData } from './utilities/firebase.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useData } from "./utilities/firebase.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-	// const [sortWage, setSortWage] = useState("")
-	const [sortDirection, setSortDirection] = useState("")
-	const [filterCategories, setFilterCategories] = useState([])
-	const [jobList, loading, error] = useData('/');
+  // const [sortWage, setSortWage] = useState("")
+  const [sortDirection, setSortDirection] = useState("");
+  const [filterCategories, setFilterCategories] = useState([]);
+  const [jobList, loading, error] = useData("/");
 
+  if (error) return <h1>{error}</h1>;
+  if (loading) return <h1>Loading your jobs...</h1>;
 
-	if (error) return <h1>{error}</h1>;
-	if (loading) return <h1>Loading your jobs...</h1>
+  const jobCategories = Array.from(
+    new Set(
+      Object.values(jobList).map((j) => {
+        return j.CATEGORY;
+      })
+    )
+  );
 
-	const jobCategories = Array.from(new Set(Object.values(jobList).map((j) => {
-		return (j.CATEGORY)
-	})))
-
-	return (
-		<div className="app-body">
-			<NavBar />
-			<div className="container">
-				<div className="filters-and-jobs">
-					<div className="filters-list">
-						<FilterMenu jobCategories={jobCategories} filterCategories={filterCategories} setFilterCategories={setFilterCategories} sortDirection={sortDirection} setSortDirection={setSortDirection} />
-					</div>
-					<div className="cards-list">
-						<JobList jobs={jobList} filterCategories={filterCategories} sortDirection={sortDirection} />
-					</div>
-				</div>
-			</div>
-		</div>
-
-	);
-}
+  return (
+    <div className="app-body">
+      <NavBar />
+      <div className="container">
+        <div className="filters-and-jobs">
+          <div className="filters-list">
+            <FilterMenu
+              jobCategories={jobCategories}
+              filterCategories={filterCategories}
+              setFilterCategories={setFilterCategories}
+              sortDirection={sortDirection}
+              setSortDirection={setSortDirection}
+            />
+          </div>
+          <div className="cards-list">
+            <JobList
+              jobs={jobList}
+              filterCategories={filterCategories}
+              sortDirection={sortDirection}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,47 +1,37 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom/extend-expect';
-import App from './App';
-import  { useData } from './utilities/firebase.js';
-jest.mock('./utilities/firebase.js');
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
+import { useData } from "./utilities/firebase.js";
 
+jest.mock("./utilities/firebase.js");
 
-test('load jobs', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Loading your jobs/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-
-const mockJobSchedule = 
-{
-  "7": {
+const mockJobSchedule = [
+  {
     "TERM AVAILABLE": "Spring 2022",
     "NUMBER OF POSITIONS AVAILABLE": "1",
-    "LOCATION": "Northwestern - Evanston Campus",
+    LOCATION: "Northwestern - Evanston Campus",
     "WORK ARRANGEMENTS": "Remote",
-    "DEPARTMENT": "American Studies",
-    "JOB DESCRIPTION": "To create a weekly newsletter for the students in the program to keep them informed about upcoming events and other things happening on campus that may be of interest.",
-    "QUALIFICATIONS": "Must be a Northwestern undergraduate student with a Federal Work-Study allotment for the 2021-2022 academic year (CAESAR > Financial Aid > View My Financial Aid > [YEAR].)",
+    DEPARTMENT: "American Studies",
+    "JOB DESCRIPTION":
+      "To create a weekly newsletter for the students in the program to keep them informed about upcoming events and other things happening on campus that may be of interest.",
+    QUALIFICATIONS:
+      "Must be a Northwestern undergraduate student with a Federal Work-Study allotment for the 2021-2022 academic year (CAESAR > Financial Aid > View My Financial Aid > [YEAR].)",
     "PAY RATE": [18.0],
     "CONTACT NAME": "Julie Lavin",
     "CONTACT PHONE NUMBER": "(847) 467-2262",
     "CONTACT EMAIL": "julie.lavin@northwestern.edu",
-    "TITLE": "American Studies: Newsletter Creation",
-    "ID": 7,
-    "URL": "https://undergradaid.northwestern.edu/work-study/jobs/on-campus-jobs/american-studies-newsletter-creation.html",
-    "CATEGORY": "Administrative"
-  }
-};
+    TITLE: "American Studies: Newsletter Creation",
+    ID: 7,
+    URL: "https://undergradaid.northwestern.edu/work-study/jobs/on-campus-jobs/american-studies-newsletter-creation.html",
+    CATEGORY: "Administrative",
+  },
+];
 
-test('title', () => {
-  console.log(useData)
+test("title", () => {
   useData.mockReturnValue([mockJobSchedule, false, null]);
   //useUserState.mockReturnValue([null]);
   render(<App />);
-  const title = screen.getByText(/myNUJob/i);
+  const title = screen.getByText(/NU/i);
   expect(title).toBeInTheDocument();
 });
-
-
-
